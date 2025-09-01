06aa8485ee18860fad35597645eed419693ed208
Ratan Kumar <krratan80@gmail.com>
2025-09-01 14:15:57 +0530

    update baseURL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://inventory-mangment-system-xyti.onrender.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
